bc588a926dae6a50872481f4e5096e9b69794d11
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-10-30 14:15:56 +0000

    fwb: Start Codeos
    Signed-off-by: Rohit Jaiswal <jaiswal.rohit6@gmail.com>

--- core/java/com/android/internal/util/codeos/AppHelper.java
+++ core/java/com/android/internal/util/codeos/AppHelper.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2013 gzosp Project
+* Copyright (C) 2017 codeos Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-package com.android.internal.util.gzosp;
+package com.android.internal.util.codeos;
 
 import android.content.Context;
 import android.content.Intent;
